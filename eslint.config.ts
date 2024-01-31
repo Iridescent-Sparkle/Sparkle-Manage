1e2b1edfe26a2ba09872442868bbb696c6ba7cb8
iridescent <2389504513@qq.com>
2024-01-31 15:48:34 +0800

    🐳 chore: 集成css、html格式化插件eslint-plugin-format

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,17 +1,21 @@
-// eslint.config.js
 import antfu from '@antfu/eslint-config'
 import { FlatCompat } from '@eslint/eslintrc'
 
 const compat = new FlatCompat()
 
 export default antfu(
-  // custom rules
   {
+    vue: true,
+    typescript: true,
+    formatters: {
+      css: true,
+      html: true,
+      markdown: 'prettier',
+    },
     rules: {
       'no-undef': 'error',
     },
   },
-  // disable no-undef rule for unplugin-auto-import
   ...compat.config({
     extends: [
       './.eslintrc-auto-import.json',
